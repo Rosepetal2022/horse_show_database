ac1214fedf299e4a7bdfac0a571dbab1c9c5138b
DusterFan668 <copelandc6@gmail.com>
2024-05-14 16:36:04 -0700

    [Implimented Horse Show Insert and Delete]

--- src/pages/HorseShows.js
+++ src/pages/HorseShows.js
@@ -1,6 +1,6 @@
 import { horseShows_data } from '../utils/sampleData';
 import { FaEdit } from "react-icons/fa";
-
+import { FaDeleteLeft } from "react-icons/fa6";
 
 function HorseShows() {
     return (
@@ -17,6 +17,7 @@ function HorseShows() {
                     <th scope="col">Prize Money Offered</th>
                     <th scope="col"># of Horses Entered</th>
                     <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -29,10 +30,41 @@ function HorseShows() {
                         <td>{horseShow.moneyOffered}</td>
                         <td>{horseShow.horsesEntered}</td>
                         <td><button className="btn btn-primary btn-sm" ><FaEdit /></button></td>
+                        <td><button className="btn btn-danger btn-sm ml-1"><FaDeleteLeft /></button></td>
                     </tr>
                 ))}
             </tbody>
         </table>
+    </div>
+    <div className="d-flex justify-content-between">
+      <div className="form-size">
+      <div className="container form-background">
+      <h2>Add Horse Show</h2>
+      <form>
+        <div className="form-group form-padding">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" id="name" defaultValue="Name" />
+        </div>
+        <div className="form-group form-padding">
+          <label htmlFor="date">Date</label>
+          <input type="date" className="form-control" id="date" />
+        </div>
+        <div className="form-group form-padding">
+          <label htmlFor="discipline">Location</label>
+          <input type="text" className="form-control" id="discipline" defaultValue="City, State" />
+        </div>
+        <div className="form-group form-padding">
+          <label htmlFor="prizeMoney">Prize Money Offered</label>
+          <input type="text" className="form-control" id="prizeMoney" defaultValue="Prize Money Offered" />
+        </div>
+        <div className="form-group form-padding">
+          <label htmlFor="prizeMoney">Number of Horses</label>
+          <input type="number" className="form-control" id="prizeMoney" defaultValue="0" />
+        </div>
+        <button type="submit" className="btn btn-primary">Add</button>
+      </form>
+    </div>
+    </div>
     </div>
     </>
     );
